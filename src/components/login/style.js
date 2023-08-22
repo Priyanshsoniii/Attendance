7c5f0e5f2006d5e4639f579fbd4183e5a2a387f4
Priyanshsoniii <priyanshsoni2001@gmail.com>
2023-08-22 08:09:50 +0530

    first

--- src/components/login/style.js
+++ src/components/login/style.js
@@ -36,6 +36,7 @@ const Wrapper = styled.section`
             
           }
           
+          
           input[type=text]:focus, input[type=password]:focus, input[type=email]:focus {
             background-color: #ddd;
             outline: none;
